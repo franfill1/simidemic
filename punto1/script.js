cc28e23ccc75fd3c35f43cf4405bb3b93c58acf1
franfill1 <francil26@gmail.com>
2021-03-28 13:30:36 +0200

    Aggiunta descrizione per ogni funzione

--- punto1/script.js
+++ punto1/script.js
@@ -1,42 +1,122 @@
+var params = 
+{
+    /*
+    parametri globali, accessibili da ogni funzione e oggetto
+    */
+    colors :
+    {
+        person :
+        {
+            suscectible : "lightGreen",
+            infected : "red",
+        },
+
+        graph :
+        {
+            suscectible : "lightGreen",
+            infected : "red",
+        },
+    },
+
+}
+
+
+
 function person()
 {
+    /*
+    person() => void
+
+    oggetto che rappresenta una persona nella simulazione
+    possiede una posizione (x,y) nella griglia di persone presente nella simulazione e uno stato, che identifica se è suscettibile o infetta
+
+    this.x => posizione x della persona nella griglia della simulazione
+    this.y => posizione y della persona nella griglia della simulazione
+    this.status => identifica lo stato della persona basato sul modello SI (suscectible, infected), questo attributo viene cambiato da funzioni esterne
+    */
+
     this.x = 0; //posizione x nella griglia
     this.y = 0; //posizione y nella griglia
 
     this.status = 0; //0 = sano, 1 = infetto
 
+    const suscectibleColor = params.colors.person.suscectible;
+    const infectedColor = params.colors.person.infected;
+
     this.updateSprite = function(canvas, R, C)
     {
+        /*
+        this.updatesprite(canvas, R, C) => void
+
+        Disegna un cerchio nel canvas, basandosi sulla posizione x e y nella griglia di R righe e C colonne
+        Il colore del cerchio dipende dallo stato (status) della persona (suscettibile/infetta)
+
+        Input: 
+        canvas => il canvas sul quale verrà disegnato il cerchio
+        R => le righe della griglia di persone associata al canvas
+        C => le colonne della griglia di persone associata al canvas
+        */
+
         var ctx = canvas.getContext("2d");
 
         var posX = canvas.width / (C + 1) * (this.x + 1);
         var posY = canvas.height / (R + 1) * (this.y + 1);
-
         if (this.status == 0)
         {
-            ctx.fillStyle = "lightGreen";
+            ctx.fillStyle = suscectibleColor;
         }
         else
         {
-            ctx.fillStyle = "red";
+            ctx.fillStyle = infectedColor;
         }
 
         ctx.beginPath();
-        ctx.arc(posX, posY, 3, 0, 2 * Math.PI);
+        ctx.arc(posX, posY, 1, 0, 2 * Math.PI);
         ctx.fill();
         ctx.stroke;
     }
 }
 
-function simulation (canvasId, R, C)
+function simulation (canvasId, Ri, Ci)
 {
+    /*
+    simulation(canvasId, R, C) => void
+    oggetto che si occupa di gestire l'intera simulazione, a intervalli scanditi esternamente
+    possiede una matrice che rappresenta una griglia di persone, disposte come verranno visualizzate sullo schermo,
+    un canvas associato, e un indice di infezione
+
+    this.grid => matrice che contiene le persone (this.grid[r][c] => oggetto di tipo person, persona che si trova nella riga r e colonna c)
+    this.canvas => canvas sul quale vanno disegnate le persone
+    this.index => indice di infezione dell'epidemia simulata
+    this.infectedN => quantità di persone infette
+    this.R => righe presenti nella griglia
+    this.C => colonne presenti nella griglia
+
+    Input:
+    canvasId => id del canvas da associare, nel documento HTML
+    Ri => numero di righe della griglia da creare
+    Ci => numero di colonne della griglia da creare
+    */
+
     this.init = function()
     {
+        /*
+        this.init() => void
+        Inizializza gli attributi dell'oggetto, basandosi sui valori di default e sugli input inseriti durante la creazione dell'oggetto
+        i valori in input di R e C vengono copiati negli appositi attributi (this.R e this.C), in modo che possano essere letti e modificati anche dall'esterno
+        La griglia (this.grid) viene ridimensionata per avere R righe e C colonne
+        Il canvas viene inizializzato in base all'id passato in input
+        Il numero di persone infette (this.infectedN) viene inizializzato a 0
+        */
+
+        this.R = Ri;
+        this.C = Ci;
+
         this.grid = [];
-        for (var i = 0; i < R; i++)
+        for (var i = 0; i < this.R; i++)
         {
             var row = [];
-            for (var j = 0; j < C; j++)
+            for (var j = 0; j < this.C; j++)
             {
                 row.push(new person());
                 row[j].x = j;
@@ -46,76 +126,108 @@ function simulation (canvasId, R, C)
         }
 
         this.canvas = document.getElementById(canvasId);
-        this.nInfected = 1;
+        this.nInfected = 0;
 
         this.index = 0.2;
-
         this.draw();
     }
 
     this.draw = function()
     {
+        /*
+        this.draw() => void
+        Disegna la griglia di persone (this.grid) sul canvas (this.canvas), prima rimuovendo ciò che era già presente 
+        */
         this.canvas.getContext("2d").clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        for (var i = 0; i < R; i++)
+        for (var i = 0; i < this.R; i++)
         {
-            for (var j = 0; j < C; j++)
+            for (var j = 0; j < this.C; j++)
             {
-                this.grid[i][j].updateSprite(this.canvas, R, C);
+                this.grid[i][j].updateSprite(this.canvas, this.R, this.C);
             }
         }
     }
 
     this.reset = function()
     {
+        /*
+        this.reset() => void
+        Rende tutte le persone nella griglia (this.grid), suscettibili, rendendo il numero totale di infetti (this.infectedN) pari a 0
+        */
         this.nInfected = 0;
-        for (var i = 0; i < R; i++)
+        for (var i = 0; i < this.R; i++)
         {
-            for (var j = 0; j < C; j++)
+            for (var j = 0; j < this.C; j++)
             {
                 this.grid[i][j].status = 0;
             }
         }
     }
-    
+
+    this.infect = function(p)
+    {
+        /*
+        this.infect(p) => void
+        Infetta la persona (p), solo se non è già infetta
+        Aggiorna di conseguenza il conteggio degli infetti (this.nInfected)
+        */
+       
+       if (p.status == 0)
+       {
+           p.status = 1;
+           this.nInfected++;
+       }
+       
+    }
+
     this.infection = function()
     {
-        var toUpdate = [];
+        /*
+        this.infection() => void
+        Simula contatti e infezioni fra la popolazione nella griglia (this.grid), in base all'indice di infezione (this.index)
+        Se la persona contrassegnata da una X è infetta, le persone contrassegnate da 0 hanno (this.index) probabilità di infettarsi
+       
+        O O O O O O O
+        O 0 0 0 O O O
+        O 0 X 0 O O O
+        O 0 0 0 O O O
+        */
 
-        for (var i = 0; i < R; i++)
+        var toInfect = [];
+
+        for (var i = 0; i < this.R; i++)
         {
-            for (var j = 0; j < C; j++)
+            for (var j = 0; j < this.C; j++)
             {
-                var kmin = i - 1, hmin = j - 1, kmax = i + 1, hmax = j + 1;
-                if (i == 0)
-                    kmin = 0;
-                if (i == R - 1)
-                    kmax = R - 1;
-                if (j == 0)
-                    hmin = 0;
-                if (j == C - 1)
-                    hmax = C - 1;             
+                
                 if (this.grid[i][j].status)
                 {
-                    for (var k = kmin; k < kmax + 1; k++)
+                    var imin = Math.max(i-1, 0), imax = Math.min(i+1, this.R - 1);
+                    var jmin = Math.max(j-1, 0), jmax = Math.min(j+1, this.C - 1);
+                    for (var ni = imin; ni <= imax; ni++)
                     {
-                        for (var h = hmin; h < hmax + 1; h++)
+                        for (var nj = jmin; nj <= jmax; nj++)
                         {
-                            if (!this.grid[k][h].status)
+                            if (!this.grid[ni][nj].status)
                             {
+
                                 if (Math.random() < this.index)
-                                    toUpdate.push(this.grid[k][h]);
+                                {
+                                    toInfect.push(this.grid[ni][nj]);
+                                }
                             }
                         }
                     }
                 }
+                
             }
         }
+        
 
-        for (var i = 0; i < toUpdate.length; i++)
+        for (var i = 0; i < toInfect.length; i++)
         {
-            if (toUpdate[i].status == 0) this.nInfected++;
-            toUpdate[i].status = 1;
+            this.infect(toInfect[i]);
         }
 
         this.draw();
@@ -124,30 +236,53 @@ function simulation (canvasId, R, C)
     this.init();
 }
 
-function graph(canvasId, dataMax)
+function graph(canvasId, dataMaxi)
 {
+    /* 
+    graph(canvasId, dataMax) => void
+    oggetto che gestisce la rappresentazione di dati raccolti sulla quantità di persone infettate ogni giorno, sottoforma di un grafico
+
+    this.canvas => canvas sul quale viene disegnato il grafico
+    this.dataMax => numerosità della popolazione
+    this.data => array contenente i dati relativi all'epidemia
+
+    Input:
+    canvasId => id nel documento HTML del canvas sul quale va disegnato il grafico
+    dataMaxi => numerosità della popolazione inizialmente
+   */
     this.init = function()
     {
+        /*
+        this.init() => void
+        Inizializza tutti gli attributi dell'oggetto
+        Il canvas (this.canvas) viene ricavato a partire dall'id fornito in input, l'array dei dati (this.data) viene svuotato
+        Il valore di (dataMaxi), dato in input, viene copiato nell'apposito attributo (this.dataMax)
+        */
         this.canvas = document.getElementById(canvasId);
+        this.canvas.style.backgroundColor = params.colors.graph.suscectible;
         this.data = [];
+        this.dataMax = dataMaxi;
     }
 
     this.draw = function()
     {
+        /*
+        this.draw() => void
+        Disegna il grafico, sull'asse delle x viene rappresentato il tempo, sull'asse delle y la quantità di infetti
+        */
         var ctx = this.canvas.getContext("2d");
         
-        ctx.fillStyle = "red";
+        ctx.fillStyle = params.colors.graph.infected;
         ctx.moveTo(0, this.canvas.height);
         ctx.beginPath();
 
         for (var i = 0; i < this.data.length; i++)
         {
-            ctx.lineTo(this.canvas.width / (this.data.length - 1) * i, this.canvas.height * (1 - (this.data[i]/dataMax)));
+            ctx.lineTo(this.canvas.width / (this.data.length - 1) * i, this.canvas.height * (1 - (this.data[i]/this.dataMax)));
         }
         ctx.lineTo(this.canvas.width, this.canvas.height);
         ctx.lineTo(0, this.canvas.height);
         ctx.fill();
-
     }
     this.init();
 }
@@ -155,9 +290,9 @@ function graph(canvasId, dataMax)
 function main()
 {
     sim = new simulation("simulationCanvas", 50, 50);
-    sim.nInfected = 1;
     gra = new graph("graph", 2500);
-    sim.grid[2][2].status = 1;
+    sim.infect(sim.grid[2][2]);
+    sim.draw();
     setInterval(update, 100);
 }
 
